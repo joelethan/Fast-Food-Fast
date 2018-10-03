f24b32af8eca3b9d12f5a5b3e04e1ab24ae81be3
joelethan <joelethan2.com>
2018-10-02 19:30:53 -0700

    Create users, menu and orders tables [Finishes #160945974]

--- api/database.py
+++ api/database.py
@@ -15,3 +15,25 @@ class DatabaseConnection:
 		except Exception as e:
 			print(e)
 			print('Failed to connect to db')
+
+
+	def create_tables(self):
+
+		""" Create all database tables"""
+
+		create_table = "CREATE TABLE IF NOT EXISTS users \
+			( user_id SERIAL PRIMARY KEY, username VARCHAR(10), \
+			email VARCHAR(100), password VARCHAR(100), admin BOOLEAN NOT NULL);"
+		self.cursor.execute(create_table)
+
+		create_table = "CREATE TABLE IF NOT EXISTS menu \
+			( food_id SERIAL PRIMARY KEY, foodname VARCHAR(15), price INTEGER);"
+		self.cursor.execute(create_table)
+
+		create_table = "CREATE TABLE IF NOT EXISTS orders \
+			( order_id SERIAL PRIMARY KEY, \
+			user_id INTEGER NOT NULL REFERENCES users(user_id), \
+			food_id INTEGER NOT NULL REFERENCES menu(food_id), \
+			quantity INTEGER, status VARCHAR(10));"
+		self.cursor.execute(create_table)
+		
