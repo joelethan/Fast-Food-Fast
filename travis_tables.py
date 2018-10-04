6766e1742f81a2451dfacfbeec7bc9b5e4e953aa
joelethan <joelethan2.com>
2018-10-04 11:14:00 -0700

    Refractor Travis files

--- travis_tables.py
+++ travis_tables.py
@@ -1,7 +1,4 @@
 from app.controllers.database import DatabaseConnection
 
-db_obj = DatabaseConnection()
-con  = db_obj.create_connection()
-db_obj.create_users_table()
-db_obj.create_menu_table()
-db_obj.create_orders_table()
+db = DatabaseConnection()
+db.create_tables()
